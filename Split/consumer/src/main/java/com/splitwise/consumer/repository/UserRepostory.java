ac2f37114b6b5452c3d40de37e7f47309f5f6a5c
B Mukesh <88962796+Wolf1712@users.noreply.github.com>
2023-07-04 15:18:12 +0530

    Delete Split/consumer directory

--- Split/consumer/src/main/java/com/splitwise/consumer/repository/UserRepostory.java
+++
@@ -1,9 +0,0 @@
-package com.splitwise.consumer.repository;
-
-import com.splitwise.consumer.model.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface UserRepostory extends JpaRepository<User,Integer> {
-}
